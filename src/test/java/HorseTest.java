9918c23c16e58151b439baa90e75a800121e652d
nickolaykla <nickolaykla@gmail.com>
2025-09-03 21:40:37 +0300

    added test classes and first test method

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class HorseTest {
+
+    @Test
+    void constructorNameCheck() {
+         Throwable exception =
+               assertThrows(IllegalArgumentException.class, () -> new Horse(null, 12.3, 123.1));
+       assertEquals("Name cannot be null.", exception.getMessage());
+    }
 }
