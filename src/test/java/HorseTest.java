790a927be0f948376f5738f3314ddaa1e3e7da3b
nickolaykla <nickolaykla@gmail.com>
2025-09-04 19:01:20 +0300

    added nameEmptyTest
    cd
    exit
    escape

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,12 +1,23 @@
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class HorseTest {
 
     @Test
-    void constructorNameCheck() {
+    void constructorNameNullCheck() {
          Throwable exception =
                assertThrows(IllegalArgumentException.class, () -> new Horse(null, 12.3, 123.1));
        assertEquals("Name cannot be null.", exception.getMessage());
     }
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\t", "\r", "\n"})
+    void constructorNameEmptyCheck(String name) {
+        Throwable exception =
+                assertThrows(IllegalArgumentException.class, () -> new Horse(name, 12.3, 123.1));
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
+
 }
